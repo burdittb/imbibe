51f9949c6ed70ef3bfd8e536d9ecc562514d6b7d
Brandie Burditt <brandieburditt@gmail.com>
2020-11-19 16:25:45 -0500

    test(materil ui): testing material-ui buttons on heroku

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { fetchDrinksByName } from '../drinkService';
 import '../stylesheets/App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div></div>
+        <div className="appButtons">
+          <Button variant="contained">Search By Name</Button>
+          <Button variant="contained">Search By Ingredient</Button>
+          <Button variant="contained">Non-Alcoholic</Button>
+          <Button variant="contained">Shake It Up</Button>
+          <Button variant="contained">Show Me Everything</Button>
+        </div>
       </BrowserRouter>
     );
   }
