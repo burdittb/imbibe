a7b58f393d705fbd020e555a96c1b7c554980335
Brandie Burditt <brandieburditt@gmail.com>
2020-11-19 18:04:25 -0500

    feat(app): add material components for testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
-import { fetchDrinksByName } from '../drinkService';
+// import { fetchDrinksByName } from '../drinkService';
 import '../stylesheets/App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="appButtons">
-          <Button variant="contained">Search By Name</Button>
-          <Button variant="contained">Search By Ingredient</Button>
-          <Button variant="contained">Non-Alcoholic</Button>
-          <Button variant="contained">Shake It Up</Button>
-          <Button variant="contained">Show Me Everything</Button>
+        <div className="app">
+          <ButtonGroup variant="contained">
+            <Button>Search By Name</Button>
+            <Button>Search By Ingredient</Button>
+            <Button>Non-Alcoholic</Button>
+            <Button>Shake It Up</Button>
+            <Button>Show Me Everything</Button>
+          </ButtonGroup>
+          <TextField variant="outlined" placeholder="search..." />
         </div>
       </BrowserRouter>
     );
--- src/components/SearchOptions.js
+++
@@ -1,9 +0,0 @@
-/* renders cute buttons for search options:
-- Search by Name
-- Search by Ingredient
-- Non-Alcoholic
-- Shake It Up
-- Show Me Everything
-*/
-
-import React from 'react';
